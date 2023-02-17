7d27f1607ddba8d3d78029c363bb53d95a5fa3f4
fabeklou <124108553+fabeklou@users.noreply.github.com>
2023-02-17 09:44:50 -0800

    Tell if the number is + , - or equal to 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
-* main - Entry point 
+* main - Entry point
 *
 * Return: Always 0 (Success)
 */
@@ -18,7 +18,7 @@ int main(void)
 		printf("%d is positive", n);
 	else if (n == 0)
 		printf("%d is zero", n);
-	else 
+	else
 		printf("%d is negative", n);
 
 	return (0);
