77fc24062677781b0b4f8350bb9a9d0c578eb357
fabeklou <124108553+fabeklou@users.noreply.github.com>
2023-02-17 18:44:16 -0800

    Print all possible combinations of two two-digit numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,45 +1,48 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Description: 'Print all possible combinations of two two-digit numbers'
+ * main - Prints numbers between 00 to 99.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	int i, j, k, l;
+int i, e, g, h, op1, op2;
 
-	for (i = '0'; i <= '9'; i++)
+i = e = g = h = 48;
+while (h < 58)
+{
+	g = 48;
+	while (g < 58)
 	{
-		for (j = '0'; j <= '9'; j++)
+		e = 48;
+		while (e < 58)
 		{
-			for (k = '0'; k <= '9'; k++)
+			i = 48;
+			while (i < 58)
 			{
-				for (l = '0'; l <= '9'; l++)
+				op1 = (h * 10) + g;
+				op2 = (e * 10) + i;
+				if (op1 < op2)
 				{
-					if ((i + j) < (k + l))
-					{
-					putchar(i);
-					putchar(j);
+					putchar(h);
+					putchar(g);
 					putchar(' ');
-					putchar(k);
-					putchar(l);
-					if (!(i == '9' && j == '8' && k == '9' && l == '9'))
-					{
-						putchar(',');
-						putchar(' ');
-						}
-					}
+					putchar(e);
+					putchar(i);
+					if (h == 57 && g == 56 && e == 57 && i == 57)
+						break;
+					putchar(',');
+					putchar(' ');
 				}
+				i++;
 			}
+			e++;
 		}
+		g++;
 	}
-
-	putchar('\n');
-
-	return (0);
+	h++;
+}
+putchar('\n');
+return (0);
 }
