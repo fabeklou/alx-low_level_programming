4fc31ba6bdaea53a731a62e77d964e1f6e4bd0e0
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-19 02:34:45 +0000

    Print the alphabet in lowercase

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,48 +1,45 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * main - Prints numbers between 00 to 99.
+ * main - Entry point
+ *
+ * Description: 'Print all possible combinations of two two-digit numbers'
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-int i, e, g, h, op1, op2;
+	int i, j, k, l;
 
-i = e = g = h = 48;
-while (h < 58)
-{
-	g = 48;
-	while (g < 58)
+	for (i = '0'; i <= '9'; i++)
 	{
-		e = 48;
-		while (e < 58)
+		for (j = '0'; j <= '9'; j++)
 		{
-			i = 48;
-			while (i < 58)
+			for (k = '0'; k <= '9'; k++)
 			{
-				op1 = (h * 10) + g;
-				op2 = (e * 10) + i;
-				if (op1 < op2)
+				for (l = '0'; l <= '9'; l++)
 				{
-					putchar(h);
-					putchar(g);
-					putchar(' ');
-					putchar(e);
+					if (((i + j) < (k + l) && k >= i) || i < k)
+					{
 					putchar(i);
-					if (h == 57 && g == 56 && e == 57 && i == 57)
-						break;
-					putchar(',');
+					putchar(j);
 					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (!(i == '9' && j == '8' && k == '9' && l == '9'))
+						{
+						putchar(',');
+						putchar(' ');
+						}
+					}
 				}
-				i++;
 			}
-			e++;
 		}
-		g++;
 	}
-	h++;
-}
-putchar('\n');
-return (0);
+
+	putchar('\n');
+
+	return (0);
 }
