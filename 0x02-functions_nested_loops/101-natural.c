5614f8566d9f34f9419b2a71670715f3213030d6
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-22 15:32:59 +0000

    Print the sum of all the multiples of 3 or 5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -9,17 +9,19 @@
 void natural(void)
 {
 	/**
-	 * @i: loop controling variable. 
+	 * @i: loop controling variable.
+	 * @f1_temp & @f2_temp: temporary variables of type float
 	 * @num: number of multiples found
 	 * @sum: sum of all the multiples
 	 */
-	int i, numb, sum;
+	int i, num, sum;
+	float f1_temp, f2_temp;
 
-	for (i = 0, sum = 0; i < 1024; i++)
+	for (i = 0, num = 0, sum = 0; i < 1024; i++)
 	{
-		i / 3;
-		(int)f_temp;
-		if (((i / 3) == (int)(i / 3)) || ((i / 5) == (int)(i / 5)))
+		f1_temp = ((float)i / 3);
+		f2_temp = ((float)i / 5);
+		if ((f1_temp == (int)f1_temp) || (f2_temp == (int)f2_temp))
 		{
 			num++;
 			sum += i;
