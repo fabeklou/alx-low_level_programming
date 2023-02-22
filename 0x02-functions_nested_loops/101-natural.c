583f847db2c5905770e769e0170541d29f275c57
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-22 15:20:50 +0000

    Print the sum of all the multiples of 3 or 5

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * natural - computes and prints the sum of all the multiples of 3 or 5
+ *
+ * Return: Void
+ */
+void natural(void)
+{
+	/**
+	 * @i: loop controling variable. 
+	 * @num: number of multiples found
+	 * @sum: sum of all the multiples
+	 */
+	int i, numb, sum;
+
+	for (i = 0, sum = 0; i < 1024; i++)
+	{
+		i / 3;
+		(int)f_temp;
+		if (((i / 3) == (int)(i / 3)) || ((i / 5) == (int)(i / 5)))
+		{
+			num++;
+			sum += i;
+		}
+	}
+
+	printf("%d", sum / num);
+}
