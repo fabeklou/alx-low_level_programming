6449286c76cc2d397ca5571256d39eba284b1616
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-22 17:44:36 +0000

    Print the first 50 Fibonacci numbers

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/**
+	 * @i: loop control variable
+	 * @acc: hold the sum the two previous numbers
+	 * @next: hold the next number to be add
+	 * @temp: temporary variable, will help to share values
+	 */
+	int  i, acc, next, temp;
+
+	for (i = 1, acc = 1, next = 0, temp = 0; i <= 50; i++)
+	{
+		temp = next;
+		next = acc;
+		acc += temp;
+
+		printf("%d", acc);
+
+		if (i != 50)
+			puts(", ");
+	}
+
+	return (0);
+}
