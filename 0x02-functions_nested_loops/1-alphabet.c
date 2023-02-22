74028406d0a3ea749136cafd0d30a9e280f2ea3d
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-22 01:35:09 +0000

    print the alphabet, in lowercase, followed by a new line

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define PRINT_ALPHABET "print_alphabet"
+
+/**
+ * main - Entry point
+ *
+ * Description: prints the alphabet in lowercase, followed by a new line.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n')
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,1 +1,5 @@
+/* prtototype of the main function */
 int main(void);
+
+/* prtototype of the functions that prints the alphabet, in lowercase, followed by a new line. */
+void print_alphabet(void);
