c7b552ff9dd173a43061b7877394beb17984d3a4
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-22 01:19:33 +0000

    Print _putchar, followed by a new line

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: prints '_putchar', followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("_putchar");
+	return (0);
+}
