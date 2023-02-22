43550970f571e9893c7dc16cb96b4856b36bce6d
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-22 07:09:58 +0000

    Print all natural numbers from n to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,34 +5,47 @@
  *
  * @n: starting point of the counter
  *
- * Return: A series of number from 'n' to 98
+ * Return: Void
  */
 void print_to_98(int n)
 {
-	while (n <= 98)
+	if (n >= 98)
 	{
-		if (n > 9)
+		for (; n > 98; n--)
 		{
 			_putchar('0' + (n / 10));
-			_putchar('0' + (n % 10));
-		}
-		else if (n < (-9))
-		{
-			_putchar('-');
-			_putchar('0' + (n / 10));
-			_putchar('0' + (n % 10));
-		}
-		else
-		{
-			_putchar('0' + n);
-		}
+			_puchar('0' + (n % 10));
 
-		if (n != 98)
-		{
 			_putchar(',');
 			_putchar(' ');
 		}
-		n++;
+		_putchar('0' + n);
+		_putchar('\n');
 	}
-	_putchar('\n');
+
+	else
+	{
+		for (; n < 98; n++)
+		{
+			if (n < -9)
+			{
+				_putchar('-');
+				_putchar('0' + (n / 10));
+				_putchar('0' + (n % 10));
+			}
+			else if (n > 9)
+				_putchar('0' + (n / 10));
+				_putchar('0' + (n % 10));
+			}
+			else
+			{
+				_putchar('0' + n);
+			}
+
+			_putchar(',');
+			_putchar(' ');
+		}
+		_puchar('0' + n);
+		_putchar('\n');
+	}
 }
