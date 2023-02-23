3f4a1d0815a2f9d60811834897ae9bde61787baf
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-23 15:08:52 +0000

    Print the numbers from 1 to 100 and Fizz, Buzz or FizzBuzz otherwise

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 1 to 100 and Fizz, Buzz or FizzBuzz otherwise
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if ((i % 15) == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if ((i % 3) == 0)
+		{
+			printf("Fizz");
+		}
+		else if ((i % 5) == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%i", i);
+		}
+
+		putchar(' ');
+	}
+	puts("");
+	return (0);
+}
