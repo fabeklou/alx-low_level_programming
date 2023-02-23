0d9a1367fc95d9bd134eab023add13ef4cdaab05
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-23 16:10:23 +0000

    Print a triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle, followed by a new line
+ *
+ * @size: size of the triangle to be printed
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int i, j, k;
+
+	for (i = size; i >= 1; i--)
+	{
+		for (j = i - 1; j >= 1; j--)
+		{
+			_putchar('.');
+		}
+
+		for (k = size - (i - 1); k >= 1; k--)
+		{
+			_putchar(35);
+		}
+
+		_putchar('\n');
+	}
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+}
