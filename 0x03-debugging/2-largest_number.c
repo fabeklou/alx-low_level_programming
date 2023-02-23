0b0dafc3f4f7dca5efbe92bc9c855c435b2a7ab2
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-23 23:55:16 +0000

    Fix the code in 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,16 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
+	int largest;
+
 	if (a > b && a > c)
 	{
-		return (a);
+		largest = a;
 	}
 	else if (b > a && b > c)
 	{
-		return (b);
+		largest = b;
 	}
 	else
 	{
-		return (c);
+		largest = c;
 	}
+
+	return (largest);
 }
