f1025e79314971cf96847cf14f77889ad56bc8b2
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-24 02:55:05 +0000

    Fix the print_remaining_days() function

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -12,26 +12,26 @@
 
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 400 == 0) || ((year % 4 == 0) && !(year % 100 == 0)))
-	{
-		if (month >= 2 && day >= 60)
-		{
-			day++;
-		}
+	int days_in_year;
 
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	{
+		/* leap year */
+		days_in_year = 366;
+		if (month > 2 && day >= 60)
+			day++;
 	}
 	else
 	{
+		/* not a leap year */
+		days_in_year = 365;
 		if (month == 2 && day == 60)
 		{
 			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
+			return;
 		}
 	}
+
+	printf("Day of the year: %d\n", day);
+	printf("Remaining days: %d\n", days_in_year - day);
 }
