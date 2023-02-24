135723ed6f6ed430f0832e9d379423fd3b348f0b
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-02-24 00:56:56 +0000

    Fix the code in 2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -1,4 +1,4 @@
-#include "hlberton.h"
+#include "main.h"
 
 /**
  * largest_number - returns the largest of 3 numbers
@@ -7,10 +7,11 @@
  * @c: third integer
  * Return: largest number
  */
-
 int largest_number(int a, int b, int c)
 {
-	/*@inter: intermediate variable (will hold the provisional largest integer)*/
+	/* 
+	 * @inter: intermediate variable (will hold the provisional largest integer)
+	 * */
 	int inter, largest;
 
 	if (a > b)
