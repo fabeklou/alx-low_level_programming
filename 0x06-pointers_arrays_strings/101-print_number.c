bd11973f54f5f3e7527b6fa75dcfbecaf3e63338
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-03 02:56:25 +0000

    Print an integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ *
+ * @n: the integer to be printed
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int n_copy = n;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n_copy = n * (-1);
+	}
+	else
+	{
+		n_copy = n;
+	}
+
+	if (n > 9)
+	{
+		print_number((n_copy / 10));
+	}
+	_putchar(n_copy % 10 + '0');
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef _MORE_POINTERS_MAIN_H
 #define _MORE_POINTERS_MAIN_H
 
+void _putchar(char c);
+
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
