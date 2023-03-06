4bcf31ee0be43f554baffe633ecf91d5960aa716
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-06 15:08:29 +0000

    locate a character in a string

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -13,10 +13,8 @@ char *_strchr(char *s, char c)
 {
 	int i;
 
-	for (i = 0; s[i] >= '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
-	return ('\0');
+	for (i = 0; (s[i] != c) && (s[i]); i++)
+		;
+
+	return (&s[i]);
 }
