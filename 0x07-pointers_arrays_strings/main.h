5ce52c0d40edc25435566ea055e4dd07fac4002b
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-06 17:45:54 +0000

    print the chessboard.

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,6 +1,8 @@
 #ifndef _HEADER_MAIN_H
 #define _HEADER_MAIN_H
 
+void _putchar(char c);
+
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
