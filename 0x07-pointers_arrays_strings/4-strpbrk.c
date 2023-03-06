87fbe147ab11f2aa55055a1c216c5a82aaa8a959
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-06 16:27:16 +0000

    searche a string for any of a set of bytes

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,7 +4,7 @@
  * _strpbrk - searches a string for any of a set of bytes
  *
  * @s: pointer to the string to be scanned
- * @sccept: pointer to the string containing the characters to match
+ * @accept: pointer to the string containing the characters to match
  *
  * Return: a pointer to the byte in s that matches one of the bytes
  * in accept, or NULL if no such byte is found
