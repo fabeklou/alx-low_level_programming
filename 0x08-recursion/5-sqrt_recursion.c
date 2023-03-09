54e16360929a82d210bf100530d7fac94a1afc55
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-09 00:00:45 +0000

    Return the natural square root of a number

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _sqrt_verify - verify en return the value of the square root
+ * if exist or (-1) otherwise
+ *
+ * @i: the smallest possible value of the square root
+ * @n: the the number wghose natural squaare root must be found
+ *
+ * Return: (-1) if the natural square root of the given number does not exist
+ * the square root otherwise
+ */
+int _sqrt_verify(int i, int n)
+{
+	if ((i * i) != n)
+	{
+		if (i == n)
+			return (-1);
+
+		return (_sqrt_verify(i + 1, n));
+	}
+	return (i);
+}
+
+
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ *
+ * @n: the number whose natural root must be found
+ *
+ * Return: (-1) if the n does not have natural square root
+ * (the natural square root) otherwise
+ */
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	if (n < 0)
+		return (-1);
+
+	if (n >= 0 && n < 100)
+		i = 1;
+	if (n >= 100 && n <= 1000)
+		i = 10;
+
+	return (_sqrt_verify(i, n));
+}
