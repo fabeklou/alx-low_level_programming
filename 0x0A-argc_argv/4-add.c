30d00c4b7ed134ae01edf5b50311702fb720f808
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-11 02:30:30 +0000

    Add a program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description - adds two positive numbers and print the result,
+ * followed by a new line
+ *
+ * @argc: argument count (number of command-line arguments)
+ * @argv: array of pointers to strings
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i = 1, sum = 0, argv_el;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+	else
+	{
+		while (i < argc)
+		{
+			argv_el = atoi(argv[i]);
+
+			if (argv_el != 0)
+			{
+				sum += argv_el;
+			}
+			else
+			{
+				puts("Error");
+				return (1);
+			}
+			i++;
+		}
+		printf("%d\n", sum);
+	}
+	return (0);
+}
