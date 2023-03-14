00612e51485feae437c9cf0bc451ef718d1d4c7c
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-14 01:30:57 +0000

    Concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,16 +11,30 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k;
+	int i, j, k = 0;
 	char *str_cat = NULL;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 
-	for (i = 0; *(s1 + i); i++)
-		;
-	for (j = 0; *(s2 + j); j++)
-		;
+	if (s1 == NULL)
+	{
+		i = 0;
+	}
+	else
+	{
+		for (i = 0; *(s1 + i); i++)
+			;
+	}
+	if (s2 == NULL)
+	{
+		j = 0;
+	}
+	else
+	{
+		for (j = 0; *(s2 + j); j++)
+			;
+	}
 
 	str_cat = (char *)malloc((sizeof(char) * (i + j)) + 1);
 
