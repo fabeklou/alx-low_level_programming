fa0740631742d15e68f4bbb1f142f67ac64fe104
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-14 11:57:52 +0000

    Concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,46 +11,38 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j, k = 0;
+	int i = 0, j = 0, k = 0, l = 0;
 	char *str_cat = NULL;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-
 	if (s1 == NULL)
-	{
-		i = 0;
 		s1 = '\0';
-	}
-	else
-	{
-		for (i = 0; *(s1 + i); i++)
-			;
-	}
-
 	if (s2 == NULL)
-	{
-		j = 0;
 		s2 = '\0';
-	}
-	else
-	{
-		for (j = 0; *(s2 + j); j++)
-			;
-	}
+
+	while (s1[i])
+		i++;
+	while (s2[j])
+		j++;
 
 	str_cat = (char *)malloc((sizeof(char) * (i + j)) + 1);
 
 	if (str_cat == NULL)
 		return (NULL);
 
-	for (k = 0; *(s1 + k); k++)
-		*(str_cat + k) = *(s1 + k);
+	while (s1[k])
+	{
+		str_cat[k] = s1[k];
+		k++;
+	}
 
-	for (i = 0; *(s2 + i); k++, i++)
-		*(str_cat + k) = *(s2 + i);
+	while (s2[l])
+	{
+		str_cat[k] = s2[l];
+		k++;
+		l++;
+	}
 
-	*(str_cat + k) = '\0';
+	str_cat[k] = '\0';
 
 	return (str_cat);
 }
