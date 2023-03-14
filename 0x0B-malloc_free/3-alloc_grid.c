9579664cf1ed1a99a171def56b8289754c4c2749
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-14 19:33:15 +0000

    Return a pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -20,21 +20,27 @@ int **alloc_grid(int width, int height)
 	grid = (int **)malloc(height * sizeof(int *));
 
 	if (grid == NULL)
-		return (NULL);
-
-	for (i = 0; i < height; i++)
-		grid[i] = (int *)malloc(width * sizeof(int));
-
-	if (grid == NULL)
 	{
 		free(grid);
 		return (NULL);
 	}
 
 	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (; i >= 0; i--)
+				free(grid[i]);
+
+			free(grid);
+			return (NULL);
+		}
+	}
+
+	for (i = 0; i < height; i++)
 		for (j = 0; j < width; j++)
 			grid[i][j] = 0;
-
 
 	return (grid);
 }
