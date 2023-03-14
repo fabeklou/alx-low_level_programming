f21eecc36b4c9b0e470e34d6d1c3e7377d953fe2
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-14 20:17:08 +0000

    Free a 2 dimensional grid

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -3,22 +3,15 @@
 /**
  * free_grid - frees a 2 dimensional grid
  *
- * @grid: pointer to the 2D grid
+ * @grid: pointer to the 2D grid to free
  * @height: size of the second dimension
  *
  * Return: nothing
  */
 void free_grid(int **grid, int height)
 {
-	if (grid == NULL)
-	{
-		exit(0);
-	}
-	else
-	{
-		for (; height >= 0; --height)
-			free(grid[height]);
+	for (height -= 1; height >= 0; height--)
+		free(grid[height]);
 
-		free(grid);
-	}
+	free(grid);
 }
