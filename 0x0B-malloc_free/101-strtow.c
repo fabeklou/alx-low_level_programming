b4869723c8aa1ea5d564df9477d00b9ac9000466
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-16 01:32:19 +0000

    Split a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,42 +1,83 @@
 #include <stdlib.h>
 
-
 /**
- * fill_str_split - fills the matrix with the words
- * cut from the target string
+ * _word_count - counts the number of words
+ * in the given string
  *
- * @str_split: pointer to the array of strings (words)
- * @words_size: pointer to an array that contains the number of
- * words and their length
- * @str: pointer to the given string
+ * @str: the string whose words are to be counted
  *
- * Return: a pointer to the array of string
+ * Return: the number of words
  */
-char **fill_str_split(char **str_split, int *words_size, char *str)
+int _word_count(char *str)
 {
-	int i, j = 0, k;
+	int i, w_count = 0;
 
 	for (i = 0; str[i]; i++)
 	{
-		if ((str[i] >= 33 && str[i] <= 126) && ((str[i - 1] == ' ') || i == 0))
+		if ((str[i] != ' ') && ((i == 0) || (str[i - 1] == ' ')))
 		{
-			for (k = 0; k < words_size[j] - 1; k++)
-			{
-				str_split[j][k] = str[i];
-				i++;
-			}
-			str_split[j][k] = '\0';
-			j++;
+			w_count++;
+			for (; str[i + 1] == ' '; i++)
+				;
 		}
+	}
+
+	return (w_count);
+}
+
+/**
+ * _char_by_word - counts the number of character
+ * for each words
+ *
+ * @str: the string to b
+ * @words_len: pointer to an array of inetger
+ * @w_count: number of words
+ *
+ * Return: nothing
+ */
+void _char_by_word(char *str, int *words_len)
+{
+	int i, c_count = 0, index = 0;
+
+	for (i = 0; str[i]; i++)
+	{
+		if ((str[i] != ' ') && ((i == 0) || (str[i - 1] == ' ')))
+		{
+			for (; str[i] && (str[i] != ' '); i++)
+				c_count++;
+			words_len[index++] = c_count;
+			c_count = 0;
+		}
+	}
+}
+
+/**
+ * _allocate_space - allocates the right space in memory
+ * for the new array of words
+ *
+ * @w_count: number of words
+ * @words_len: array of the words length
+ *
+ * Return: pointer to the allocated memory space
+ */
+char **_allocate_space(int w_count, int *words_len)
+{
+	int i;
+	char **str_split = (char **)calloc(w_count + 1, sizeof(char *));
+
+	for (i = 0; i < w_count + 1; i++)
+	{
+		if (i == w_count)
+			str_split[i] = (char *)calloc(1, sizeof(char));
+		else
+			str_split[i] = (char *)calloc(words_len[i] + 1, sizeof(char));
 	}
 
 	return (str_split);
 }
 
-
-
 /**
- * strtow - splits a string into words
+ * strtow - splits string into words
  *
  * @str: pointer to the string to split
  *
@@ -44,44 +85,34 @@ char **fill_str_split(char **str_split, int *words_size, char *str)
  */
 char **strtow(char *str)
 {
-	int i, k = 0, l = 0, len = 0;
+	int i, x = 0, y = 0;
+	int w_count;
+	int *words_len = NULL;
 	char **str_split = NULL;
-	int *words_size = NULL;
 
-	if (str == NULL)
+	w_count = _word_count(str);
+
+	if (str == NULL || w_count == 0)
 		return (NULL);
 
-	for (i = 0; str[i]; i++)
-		if ((str[i] >= 33 && str[i] <= 126) && ((str[i - 1] == ' ') || i == 0))
-			k++;
+	words_len = (int *)calloc(w_count, sizeof(int));
+	_char_by_word(str, words_len);
 
-	str_split = (char **)malloc((sizeof(char *) * k) + 1);
-	if (str_split == NULL)
-		free(str_split);
-	words_size = (int *)calloc(k, sizeof(int));
-	if (words_size == NULL)
-		free(words_size);
+	str_split = _allocate_space(w_count, words_len);
 
 	for (i = 0; str[i]; i++)
 	{
-		if ((str[i] >= 33 && str[i] <= 126) && ((str[i - 1] == ' ') || i == 0))
+		if ((str[i] != ' ') && ((i == 0) || (str[i - 1] == ' ')))
 		{
-			for (len = 0; str[i] != ' '; i++)
-				len++;
-			words_size[l] = len + 1;
-			l++;
+			for (; str[i] && (str[i] != ' '); i++)
+				str_split[x][y++] = str[i];
+			str_split[x][y] = '\0';
+			y = 0;
+			x++;
 		}
 	}
+	str_split[x] = NULL;
+	free(words_len);
 
-	for (i = 0; i < k; i++)
-	{
-		str_split[i] = (char *)malloc((sizeof(char)) * words_size[0]);
-		if (str_split[i] == NULL)
-		{
-			for (; i >= 0; i--)
-				free(str_split[i]);
-			free(str_split);
-		}
-	}
-	return (fill_str_split(str_split, words_size, str));
+	return (str_split);
 }
