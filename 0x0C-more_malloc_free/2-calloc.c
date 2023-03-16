5ff1b913843a66468ee7de57f4009fdfb678a810
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-16 14:43:43 +0000

    Allocate memory for an array, using malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,28 +13,15 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	unsigned int i;
-	void **p = NULL;
+	char *p = NULL;
 
 	if (nmemb == 0  || size == 0)
 		return (NULL);
 
-	p = malloc(nmemb);
+	p = malloc(nmemb * size);
 
 	if (p == NULL)
 		return (NULL);
-
-	for (i = 0; i < nmemb; i++)
-	{
-		p[i] = malloc(size);
-
-		if (p[i] == NULL)
-		{
-			for (; i > 0; i--)
-				free(p[i]);
-			free(p);
-			return (NULL);
-		}
-	}
 
 	for (i = 0; i < nmemb * size; i++)
 		p[i] = 0;
