2869d4e002fb34077d4626b33a00273613892c1a
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-16 13:37:22 +0000

    Concatenate two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -36,7 +36,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (p == NULL)
 		return (NULL);
 
-	for (i = 0; i < count_s2; i++)
+	for (i = 0; i < count_s1; i++)
 		p[i] = s1[i];
 
 	for (j = 0; j < n; j++)
