2e0423e32eb4fdf5aae1b25f936751bae4619175
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-20 22:53:32 +0000

    Define a new type 'struct dog'

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,17 @@
 #ifndef _DOG_
 #define _DOG_
 
+/**
+ * struct dog - defines a new type <struct dog> with
+ * the following elements:
+ *
+ * @name: dog's member of type pointer to character
+ * @age: dog's member of type float
+ * @owner: dog's member of type pointer to character
+ *
+ * Description: defines a new type <struct dog> with
+ * @name @age and @owner
+ */
 struct dog
 {
 		char *name;
