16756a67e2fd856370bdf4c501c890af2d908f61
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-21 00:03:05 +0000

    Initialize a variable of type 'struct dog'

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -13,11 +13,6 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-		d = malloc(sizeof(struct dog));
-
-		if (d == NULL)
-				exit(1);
-
 		d->name = name;
 		d->owner = owner;
 		d->age = age;
