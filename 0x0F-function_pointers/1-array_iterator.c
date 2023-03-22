54bd8cfee1e386752d4350e83c56de73d3931fe1
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-22 14:28:28 +0000

    Execute a callback function on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (action != NULL && array == NULL && size > 0)
+	if (action != NULL && array != NULL && size > 0)
 		for (i = 0; i < size; i++)
 			action(array[i]);
 }
