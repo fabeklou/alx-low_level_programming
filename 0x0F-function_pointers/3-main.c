9c3866c56847dc43c4f5f7051af1817b05c79b25
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-22 19:28:16 +0000

    Add a program that performs simple operations

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,39 @@
+#include "3-calc.h"
+
+/**
+ * main - entry point
+ *
+ * @argc: number of arguments
+ * @argv: array of command-line arguments
+ *
+ * Return: always 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int num_1 = atoi(argv[1]), num_2 = atoi(argv[3]);
+	int (*ptr_opf)(int, int);
+
+	if (argc != 4)
+	{
+		puts("Error");
+		exit(98);
+	}
+
+	ptr_opf = get_op_func(argv[2]);
+
+	if (ptr_opf == NULL)
+	{
+		puts("Error");
+		exit(99);
+	}
+
+	if ((!(strcmp(argv[2], "/")) || !(strcmp(argv[2], "%"))) && num_2 == 0)
+	{
+		puts("Error");
+		exit(100);
+	}
+
+	printf("%d\n", ptr_opf(num_1, num_2));
+
+	return (0);
+}
