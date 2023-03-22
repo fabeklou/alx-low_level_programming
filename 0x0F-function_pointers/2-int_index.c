638ae36d9d854b5bdc8c79898660e89949d90db6
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-22 14:58:57 +0000

    Search for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -21,10 +21,10 @@ int int_index(int *array, int size, int (*cmp)(int))
 	if (array != NULL && cmp != NULL)
 	{
 		for (i = 0; i < size; i++)
-			if (cmp(array[i]) == 1)
+			if (cmp(array[i]) != 0)
 				return (i);
 
 		return (-1);
 	}
-	return (0);
+	return (-1);
 }
