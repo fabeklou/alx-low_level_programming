f4d3a389247475c12181003b9be7805ca6fea82b
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-24 02:44:00 +0000

    Add a function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+
+/**
+  * print_char - prints a character
+  *
+  * @args: list of variadic arguments
+  *
+  * Return: nothing
+  */
+void print_char(va_list args)
+{
+	printf("%c", (char)va_arg(args, int));
+}
+
+
+/**
+  * print_integer - prints an integer
+  *
+  * @args: list of variadic arguments
+  *
+  * Return: nothing
+  */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+
+/**
+  * print_float - prints a float
+  *
+  * @args: A list of variadic arguments
+  *
+  * Return: nothing
+  */
+void print_float(va_list args)
+{
+	printf("%f", (float)va_arg(args, double));
+}
+
+
+/**
+  * print_string - prints a string
+  *
+  * @args: A list of variadic arguments
+  *
+  * Return: nothing
+  */
+void print_string(va_list args)
+{
+	char *str = va_arg(args, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+
+/**
+ * print_all - prints anything (char, integer, float, char *)
+ *
+ * @format: a string representing the list of types of arguments
+ * passed to the function
+ * @...: variadic arguments
+ *
+ * Return: nothing
+ */
+void print_all(const char * const format, ...)
+{
+	int i = 0, j = 0;
+	va_list args;
+	char *separator = "";
+	ft_dt valid_id[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(args, format);
+
+	while (format != NULL && format[i])
+	{
+		j = 0;
+
+		while (j < 4 && format[i] != *valid_id[j].id)
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			valid_id[j].f(args);
+			separator = ", ";
+		}
+		i++;
+	}
+
+	va_end(args);
+	printf("\n");
+}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,9 +1,25 @@
-#ifndef _VARIADIC_FUNC
-#define _VARIADIC_FUNC
+#ifndef _VARIADIC_FUNC_H
+#define _VARIADIC_FUNC_H
+
+#include <stdarg.h>
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
-/* End of _VARIADIC_FUNC */
+/**
+ * struct ft_dt - struct ft_dt
+ *
+ * @identifier: The conversion specifier
+ * @f: The function associated
+ */
+typedef struct ft_dt
+{
+	char *id;
+	void (*f)(va_list args);
+} ft_dt;
+
+void print_all(const char * const format, ...);
+
+/* End of _VARIADIC_FUNC_H */
 #endif
