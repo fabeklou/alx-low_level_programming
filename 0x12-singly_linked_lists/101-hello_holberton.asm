9253e377e1d064a2638f70e08f18b8ecababb567
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-03-31 00:41:22 +0000

    Print Hello, Holberton

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,23 @@
+bits    64
+default rel
+
+global  main
+
+extern  printf
+
+section .data
+    format      db 'Hello, Holberton!', 0xA, 0
+
+section .bss
+
+section .text
+    main:
+        sub     rsp, 8
+
+        lea     rdi, [format]
+        mov     al, 0
+        call    printf wrt ..plt
+
+        add     rsp, 8
+        sub     rax, rax
+        ret
