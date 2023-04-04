5c96b17d38b4ed45b900d8b66a81a52cfa036a67
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-04-04 19:35:48 +0000

    Free a listint_t list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a listint_t list
+ *
+ * @head: reference to the head node
+ *
+ * Return: nothing
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *next_node;
+
+	while (*head)
+	{
+		next_node = (*head)->next;
+		free(*head);
+		*head = next_node;
+	}
+}
