b515518ac816037c69b6e27b8baf17ab72adcd23
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-04-06 02:09:37 +0000

    Safely free a linked list, using (Floyd's Algo)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,76 @@
+#include "lists.h"
+
+/**
+ * find_looped_size - search for a possible loop in a single
+ * linked list and return the length if found
+ *
+ * @head: pointer to the head node
+ *
+ * Return: number of nodes if looped, 0 otherwise
+ */
+size_t find_looped_size(const listint_t *head)
+{
+	size_t node_count = 0;
+	const listint_t *walking_ptr = head, *jumping_ptr = head;
+
+	if (!head || !(head->next))
+		return (0);
+
+	while (walking_ptr && jumping_ptr && jumping_ptr->next)
+	{
+		walking_ptr = walking_ptr->next;
+		jumping_ptr = jumping_ptr->next->next;
+
+		node_count++;
+
+		if (walking_ptr == jumping_ptr)
+		{
+			while (walking_ptr != head)
+			{
+				walking_ptr = walking_ptr->next;
+				head = head->next;
+
+				node_count++;
+			}
+			return (node_count);
+		}
+	}
+	return (0);
+}
+
+/**
+ * free_listint_safe - frees a listint_t list
+ *
+ * @h: reference to the head node
+ *
+ * Return: the size of the list that was free’d
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	size_t node_count = 0, valid_node;
+	listint_t *current_node = *h, *temp_node = *h;
+
+	node_count = find_looped_size(*h);
+
+	if (node_count)
+	{
+		for (valid_node = 0; valid_node < node_count; valid_node++)
+		{
+			temp_node = current_node->next;
+			free(current_node);
+			current_node = temp_node;
+		}
+		*head = NULL;
+	}
+	else
+	{
+		for (; current_node; node_count++)
+		{
+			temp_node = current_node->next;
+			free(current_node);
+			current_node = temp_node;
+		}
+	}
+	h = NULL;
+	return (node_count);
+}
