691c8ead669f483cdd8a5fd043cc1b857ba4584c
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-04-10 12:21:38 +0000

    Return the value of a bit at a given index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	if (index == 0)
+	{
+		if (n & 1)
+			return (1);
+		else
+			return (0);
+	}
+
+	return (get_bit(n >> 1, index - 1));
+}
--- 0x14-bit_manipulation/_test_files/2-main.c
+++ 0x14-bit_manipulation/_test_files/2-main.c
@@ -16,5 +16,7 @@ int main(void)
     printf("%d\n", n);
     n = get_bit(1024, 0);
     printf("%d\n", n);
+	n = get_bit(1, 4);
+	printf("%d\n", n);
     return (0);
 }
