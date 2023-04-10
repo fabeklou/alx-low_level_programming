781914d18ef9bf10bbe832087b002285d37d05ec
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-04-10 12:07:52 +0000

    Print the binary representation of a number <recursively>

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,37 +1,45 @@
 #include "main.h"
 
 /**
+ * _shift_print_back - shifts and prints a binary number
+ * backword using recursion
+ *
+ * @num: the decimal number >= 2
+ *
+ * Rteurn: nothing
+ */
+void _shift_print_back(unsigned long int num)
+{
+	if (num == 0)
+		return;
+
+	_shift_print_back(num >> 1);
+
+	if (num & 1)
+		_putchar(49);
+	else
+		_putchar(48);
+}
+
+/**
  * print_binary - prints the binary representation of a number
  *
  * @n: the decimal number
  *
- * Return : nothing
+ * Return: nothing
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int lar_bit_w = 1;
-
 	if (n == 0)
 	{
-		_putchar('0');
+		_putchar(48);
 		return;
 	}
+
 	if (n == 1)
 	{
-		_putchar('1');
+		_putchar(49);
 		return;
 	}
-
-	while (lar_bit_w * 2 <= n)
-		lar_bit_w *= 2;
-
-	while (lar_bit_w > 0)
-	{
-		if (n == lar_bit_w ||  n > lar_bit_w)
-			n = (_putchar('1'), n - lar_bit_w);
-		else
-			_putchar('0');
-
-		lar_bit_w *= 0.5;
-	}
+	 _shift_print_back(n);
 }
