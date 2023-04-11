918d32015b1a8bd9f6dcfd369ebc38043b13bd90
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-04-11 01:09:17 +0000

    Read a text file and prints it

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif /* End of _MAIN_H */
