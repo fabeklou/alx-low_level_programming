43c7d3b1531baeb4105de3ae8b75d299ba79f55b
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-07-11 00:24:26 +0000

    Read a text file and prints it to the stdout

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,29 +11,29 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-    ssize_t _open, _read, _write;
-    char *buffer;
+	ssize_t _open, _read, _write;
+	char *buffer;
 
-    if (!filename)
-        return (0);
+	if (!filename)
+		return (0);
 
-    buffer = (char *)malloc(sizeof(char) * letters);
+	buffer = (char *)malloc(sizeof(char) * letters);
 
-    if (!buffer)
-        return (0);
+	if (!buffer)
+		return (0);
 
-    _open = open(filename, O_RDONLY);
-    _read = read(_open, buffer, letters);
-    _write = write(STDOUT_FILENO, buffer, _read);
+	_open = open(filename, O_RDONLY);
+	_read = read(_open, buffer, letters);
+	_write = write(STDOUT_FILENO, buffer, _read);
 
-    if (_open == -1 || _read == -1 || _write == -1 || _read != _write)
-    {
-        free(buffer);
-        return (0);
-    }
+	if (_open == -1 || _read == -1 || _write == -1 || _read != _write)
+	{
+		free(buffer);
+		return (0);
+	}
 
-    free(buffer);
-    close(_open);
+	free(buffer);
+	close(_open);
 
-    return (_write);
+	return (_write);
 }
