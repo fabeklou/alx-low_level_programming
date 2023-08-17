6dade66ebde069c824da26bfb3b833bb26d8abd8
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-08-17 09:31:56 +0000

    Add a new node at the beginning of a list : dbg

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -14,12 +14,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new = malloc(sizeof(dlistint_t));
 
-	if (new == NULL)
+	if (head == NULL || new == NULL)
 		return (NULL);
 
 	new->n = n;
 	new->next = *head;
 	new->prev = NULL;
+
+	if (*head)
+		(*head)->prev = new;
+
+	*head = new;
 
 	return (new);
 }
