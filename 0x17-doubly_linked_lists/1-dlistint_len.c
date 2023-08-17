f1a62b37b8e759ded00704d574f16058849b1b56
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-08-17 09:00:30 +0000

    Return the number of elements in a linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - returns the number of elements in a linked dlistint_t list
+ *
+ * @h: pointer to the head node
+ *
+ * Return: the number of elements in the given linked
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
