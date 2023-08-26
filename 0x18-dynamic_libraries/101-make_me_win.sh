c168bfedd7fa8b09486ac901fae21711caf76626
Fabrice Eklou <124108553+fabeklou@users.noreply.github.com>
2023-08-26 02:06:36 +0000

    Overwrite rand() function to win the Giga Millions : ut

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/fabeklou/alx-low_level_programming/blob/main/0x18-dynamic_libraries/iam_lucky.so
-export LD_PRELOAD=/tmp/iam_lucky.so
+wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/putshack.so
+export LD_PRELOAD=/tmp/putshack.so
